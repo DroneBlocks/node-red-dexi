6d6d5daa25eb2008c3690ff380d58199f79ad20e
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-10-24 14:23:51 -0500

    Allow color passing to LED node via string

--- src/led.js
+++ src/led.js
@@ -3,11 +3,14 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config)
         var node = this
         node.on('input', function(msg) {
+            // Use color from message payload if available, otherwise use config
+            const color = (msg.payload && msg.payload.color) ? msg.payload.color : config.color
+
             msg.payload = {
                 "serviceName": "/dexi/led_service/set_led_ring_color",
                 "serviceType": "dexi_interfaces/srv/LEDRingColor",
                 "serviceRequest": {
-                    "color": config.color
+                    "color": color
                 }
             }
             node.send(msg)
