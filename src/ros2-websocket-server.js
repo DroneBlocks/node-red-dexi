9c69c6ddd67d13f2f7eb0f31742c4e99e1475feb
Casey Hanner <bhanner95@gmail.com>
2025-12-05 14:44:00 -0600

    make topics dynamic

--- src/ros2-websocket-server.js
+++ src/ros2-websocket-server.js
@@ -1,69 +1,82 @@
-module.exports = function (RED){
-    var ROSLIB = require('roslib');
+module.exports = function (RED) {
+  var ROSLIB = require('roslib');
 
-    let dexiTopics = []
-  
-    function ROS2WebsocketServerNode(config) {
-      RED.nodes.createNode(this, config);
-      var node = this;
+  let dexiTopics = []
 
-      node.closing = false;
-      node.on("close", function() {
-        node.closing = true;
-        if (node.tout) { clearTimeout(node.tout); }
-        if (node.ros){
-          node.ros.close();
-        }
+  function ROS2WebsocketServerNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+
+    node.closing = false;
+    node.on("close", function () {
+      node.closing = true;
+      if (node.tout) { clearTimeout(node.tout); }
+      if (node.topicInterval) { clearInterval(node.topicInterval); }
+      if (node.ros) {
+        node.ros.close();
+      }
+    });
+
+    var trials = 0;
+
+    function startconn() {    // Connect to remote endpoint
+      var ros = new ROSLIB.Ros({
+        url: config.url
       });
-  
-      var trials = 0;
-  
-      function startconn() {    // Connect to remote endpoint
-        var ros = new ROSLIB.Ros({
-          url : config.url
-        });
-        node.ros = ros; // keep for closing
-        handleConnection(ros);
-      }
-  
-      function handleConnection(ros) {
-        ros.on('connection', function() {
-          node.emit('ros connected');
-          node.log('connected');
-
-          // Get list of topics to send to the editor for ros2 pub/sub nodes
-          node.ros.getTopics((topicsResponse) => {
-            dexiTopics = topicsResponse
-          })
-
-        });
-  
-        ros.on('error', function(error) {
-          trials++;
-          node.emit('ros error');
-          node.log('Error connecting : ', error);
-
-          //if(trials == 5) node.closing = true
-        });
-  
-        ros.on('close', function() {
-            node.emit('ros closed');
-            node.log('Connection closed');
-          if (!node.closing) {
-            node.log('reconnecting');
-            node.tout = setTimeout(function(){ startconn(); }, 1000);
-          }
-        });
-      }
-  
-      startconn();
-      node.closing = false;
+      node.ros = ros; // keep for closing
+      handleConnection(ros);
     }
 
-    // Expose "API" to the editor for displaying topics
-    RED.httpAdmin.get('/dexi/topics', (req, res) => {
-      res.json(dexiTopics)
-    })
-  
-    RED.nodes.registerType("ros2-websocket-server", ROS2WebsocketServerNode);
-  };
+    function handleConnection(ros) {
+      ros.on('connection', function () {
+        node.emit('ros connected');
+        node.log('connected');
+
+        // Get list of topics to send to the editor for ros2 pub/sub nodes
+        // Initial fetch
+        node.ros.getTopics((topicsResponse) => {
+          dexiTopics = topicsResponse
+        })
+
+        // Poll for changes every 5 seconds
+        if (node.topicInterval) clearInterval(node.topicInterval);
+        node.topicInterval = setInterval(() => {
+          if (node.ros) {
+            node.ros.getTopics((topicsResponse) => {
+              dexiTopics = topicsResponse
+            })
+          }
+        }, 5000);
+
+      });
+
+      ros.on('error', function (error) {
+        trials++;
+        node.emit('ros error');
+        node.log('Error connecting : ', error);
+
+        //if(trials == 5) node.closing = true
+      });
+
+      ros.on('close', function () {
+        if (node.topicInterval) { clearInterval(node.topicInterval); }
+        node.emit('ros closed');
+        node.log('Connection closed');
+        if (!node.closing) {
+          node.log('reconnecting');
+          node.tout = setTimeout(function () { startconn(); }, 1000);
+        }
+      });
+    }
+
+    startconn();
+    node.closing = false;
+  }
+
+  // Expose "API" to the editor for displaying topics
+  RED.httpAdmin.get('/dexi/topics', (req, res) => {
+    res.json(dexiTopics)
+  })
+
+  RED.nodes.registerType("ros2-websocket-server", ROS2WebsocketServerNode);
+};
